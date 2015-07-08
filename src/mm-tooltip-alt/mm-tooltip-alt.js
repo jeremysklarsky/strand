aaaa7cd7814661c66f8d59f1e1bdceb7b827519e
Anthony Koerber <akoerber@mediamath.com>
2015-07-08 15:49:58 -0500

    alt tooltip approach

---
+++ src/mm-tooltip-alt/mm-tooltip-alt.js
@@ -0,0 +1,100 @@
+(function(scope) {
+
+	scope.Tooltip = Polymer({
+		is: 'mm-tooltip-alt',
+
+		behaviors: [
+			StrandTraits.AutoClosable,
+			StrandTraits.Stackable,
+			StrandTraits.PositionableTip,
+			StrandTraits.Stylable
+		],
+
+		properties: {
+			CLOSE_ICON_COLOR: {
+				type: String,
+				value: Colors.A4
+			},
+			CLOSE_ICON_HOVER: {
+				type: String,
+				value: Colors.F0
+			},
+			auto: { 
+				type: Boolean,
+				value: false, 
+				refelctToAttribute: true,
+				observer: '_autoChanged'
+			},
+			direction: {
+				value: 'n'
+			},
+			tipWidth: {
+				type: Number,
+				value: false, // if not set, assume it should be the width of it's content
+				refelctToAttribute: true,
+				observer: '_widthChanged'
+			}
+		},
+
+		attached: function() {
+			this.async(function() {
+				if (this.target) {
+					this.target.addEventListener('mouseover', this.overHandler.bind(this));
+					this.target.addEventListener('mouseout', this.outHandler.bind(this));
+					this.target.style.cursor = 'pointer';
+				}
+			});
+		},
+
+		removed: function() {
+			if (this.target) {
+				this.target.removeEventListener('mouseover', this.overHandler.bind(this));
+				this.target.removeEventListener('mouseout', this.outHandler.bind(this));
+				this.target.style.cursor = 'default';
+			}
+		},
+
+		open: function(e) {
+			this.state = this.STATE_OPENED;
+		},
+
+		close: function(e) {
+			this.state = this.STATE_CLOSED;
+		},
+
+		overHandler: function(e) {
+			this.open();
+		},
+
+		outHandler: function(e) {
+			if(!this.auto) {
+				this.close();
+			}
+		},
+
+		_widthChanged: function(newVal, oldVal) {
+			this.style.width = newVal + 'px';
+		},
+
+		_autoChanged: function(newVal, oldVal) {
+			if (newVal && typeof newVal === 'string') {
+				newVal = JSON.parse(newVal.toLowerCase());
+			}
+		},
+
+		_updateClass: function(auto) {
+			var o = {};
+			o.auto = auto;
+			return this.classBlock(o);
+		},
+
+		_closeFilter: function(instance, e, original) {
+			var closeIcon = instance.$$('.close-icon');
+			if(e.path.indexOf(closeIcon) > -1){
+				instance.close();
+			}
+		}
+
+	});
+
+})(window.Strand = window.Strand || {});
